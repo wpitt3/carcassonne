cc3dfe5f77beb8be1b2f7126413f1a81fa0fc75d
wpitt <wpitt3@gmail.com>
2023-09-03 14:38:36 +1200

    tidying display and updating todo.md

--- src/tiles/TileDisplay.tsx
+++ src/tiles/TileDisplay.tsx
@@ -3,27 +3,27 @@ import isEqual from 'lodash/isEqual';
 
 import './TileDisplay.css';
 import { Tile } from './TileStore';
+import cathedral from '../assets/cathedral.png';
+import city_full from '../assets/city_full.png';
+import city_half from '../assets/city_half.png';
+import city_opposite from '../assets/city_opposite.png';
+import city_side from '../assets/city_side.png';
+import city_three from '../assets/city_three.png';
+import garden_mid from '../assets/garden_mid.png';
+import garden_top from '../assets/garden_top.png';
+import grass_side from '../assets/grass_side.png';
+import inn_bottom_right from '../assets/inn_bottom_right.png';
+import inn_top from '../assets/inn_top.png';
 import monastery from '../assets/monastery.png';
-import a_road from '../assets/1-road.png';
-import c_road from '../assets/c-road.png';
+import river_curved from '../assets/river_curved.png';
+import river_end from '../assets/river_end.png';
+import river_kinked from '../assets/river_kinked.png';
+import river_straight from '../assets/river_straight.png';
+import road_curved from '../assets/road_curved.png';
+import road_half from '../assets/road_half.png';
+import shield_mid from '../assets/shield_mid.png';
+import shield_top from '../assets/shield_top.png';
 import village from '../assets/village.png';
-import a_city from '../assets/1-city.png';
-import all_city from '../assets/all-city.png';
-import half_city from '../assets/half-city.png';
-import three_city from '../assets/3-city.png';
-import b_city from '../assets/b-city.png';
-import a_grass from '../assets/1-grass.png';
-import top_shield from '../assets/top-shield.png';
-import mid_shield from '../assets/mid-shield.png';
-import t_garden from '../assets/t-garden.png';
-import m_garden from '../assets/m-garden.png';
-import b_river from '../assets/b-river.png';
-import c_river from '../assets/c-river.png';
-import e_river from '../assets/e-river.png';
-import s_river from '../assets/s-river.png';
-import cathedral from '../assets/cat.png';
-import bottom_right_inn from '../assets/b-r-inn.png';
-import on_top_inn from '../assets/mid-inn.png';
 
 interface Params {
     tile: Tile
@@ -43,23 +43,23 @@ const multiImage = (...images: React.JSX.Element[]): React.JSX.Element => {
 
 const roads = (r: number[], c: number[]) => {
     if (isEqual(r, [0, 0, 1, 0]) && isEqual(c,[1, 0, 0, 0])) {
-        return multiImage(image(a_road), image(a_road, 2));
+        return multiImage(image(road_half), image(road_half, 2));
     }
     if (isEqual(r, [0, 1, 0, 2]) && isEqual(c,[1, 0, 2, 0])) {
-        return multiImage(image(a_road), image(a_road, 1), image(a_road, 2), image(a_road, 3), image(village));
+        return multiImage(image(road_half), image(road_half, 1), image(road_half, 2), image(road_half, 3), image(village));
     }
 
     const toPrint: React.JSX.Element[] = [];
     if (r.filter(num => num === 1).length == 2 && (r[0] === r[1] || r[1] === r[2])) {
         for (let i = 0; i < 4; i++) {
             if (!!r[i] && r[i] === r[(i+1)%4]) {
-                toPrint.push(image(c_road, ((i+2)%4)));
+                toPrint.push(image(road_curved, ((i+2)%4)));
             }
         }
     } else {
         for (let i = 0; i < 4; i++) {
             if (!!r[i]) {
-                toPrint.push(image(a_road, ((i+2)%4)));
+                toPrint.push(image(road_half, ((i+2)%4)));
             }
         }
         if(r.filter(num => num === 2).length > 0) {
@@ -71,71 +71,71 @@ const roads = (r: number[], c: number[]) => {
 
 const cities = (c: number[], f: number[]) => {
     if (isEqual(c,[1, 0, 2, 1])) {
-        return multiImage(image(half_city), image(a_city, 2));
+        return multiImage(image(city_half), image(city_side, 2));
     }
 
     if (isEqual(c,[0, 0, 0, 1]) && f[1] !== f[2]) {
-        return multiImage(image(half_city), image(a_grass));
+        return multiImage(image(city_half), image(grass_side));
     }
 
     if (c.filter(num => num === 1).length < 2) {
         const toPrint: React.JSX.Element[] = [];
         for (let i = 0; i < 4; i++) {
             if (!!c[i]) {
-                toPrint.push(image(a_city, i));
+                toPrint.push(image(city_side, i));
             }
         }
         return multiImage(...toPrint);
     }
 
     if (isEqual(c, [1, 1, 1, 1])) {
-        return image(all_city);
+        return image(city_full);
     }
     if (isEqual(c, [1, 0, 0, 1])) {
-        return image(half_city);
+        return image(city_half);
     }
     if (isEqual(c, [0, 1, 0, 1])) {
-        return image(b_city);
+        return image(city_opposite);
     }
     if (isEqual(c, [1, 1, 0, 1])) {
-        return image(three_city, 2);
+        return image(city_three, 2);
     }
 }
 
 const shield = (c: number[]) => {
     if (isEqual(c, [0, 1, 0, 1])) {
-        return image(mid_shield);
+        return image(shield_mid);
     }
-    return image(top_shield);
+    return image(shield_top);
 }
 
 const garden = (r: number[], c: number[] ) => {
     if(!isEqual(r, [])) {
-        return !r[1] ? image(t_garden, 1) : image(t_garden, 2);
+        return !r[1] ? image(garden_top, 1) : image(garden_top, 2);
     }
     if( c.length > 0 && !!c[0] && !!c[2]) {
-        return image(m_garden);
+        return image(garden_mid);
     }
-    return image(t_garden, 2);
+    return image(garden_top, 2);
 }
 
 const river = (ri: boolean[], m: boolean ) => {
     if (isEqual(ri, [false, false, true, false])) {
-        return image(e_river);
+        return image(river_end);
     }
     if (isEqual(ri, [false, true, false, true])) {
-        return m ? image(b_river) : image(s_river);
+        return m ? image(river_kinked) : image(river_straight);
     }
     if (isEqual(ri, [false, true, true, false])) {
-        return image(c_river, 3);
+        return image(river_curved, 3);
     }
     if (isEqual(ri, [true, true, false, false])) {
-        return image(c_river, 2);
+        return image(river_curved, 2);
     }
 }
 
 const inn = (hasCities: boolean) => {
-    return hasCities ? image(bottom_right_inn) : image(on_top_inn);
+    return hasCities ? image(inn_bottom_right) : image(inn_top);
 }
 
 function TileDisplay( {tile}: Params ) {
